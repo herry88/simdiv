c3c014f59272cfd2440e926f59d273dc4f3252a1
Herry Prasetyo <herry081288@gmail.com>
2021-12-05 09:46:11 +0700

    first commit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,39 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Simdiv</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">SD</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown active">
+                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+
+            </li>
+
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Data Master</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#">Data Category</a></li>
+                    <li><a class="nav-link" href="#">Data Tahun Akademik</a></li>
+                    <li><a class="nav-link" href="#">Data Dosen</a></li>
+                    <li><a class="nav-link" href="#">Data Staff</a></li>
+                    <li><a class="nav-link" href="#">Data Inventory</a></li>
+
+                </ul>
+            </li>
+            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a>
+            </li>
+
+        </ul>
+
+        <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                <i class="fas fa-rocket"></i> Documentation
+            </a>
+        </div>
+    </aside>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//rute
+Route::get('hello', function () {
+    return '<h1>Hello Laravel</h1>';
+});
+
+//rute category
+Route::resource('category', App\Http\Controllers\CategoryController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
